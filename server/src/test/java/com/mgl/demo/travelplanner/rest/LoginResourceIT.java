067e1320590e8289ffdfe955750cd45bda7a5d32
Miguel García López <miguel.garcia@stelsolutions.com>
2016-12-23 20:22:06 +0100

    Add more tests for registration and login.

--- server/src/test/java/com/mgl/demo/travelplanner/rest/LoginResourceIT.java
+++ server/src/test/java/com/mgl/demo/travelplanner/rest/LoginResourceIT.java
@@ -12,13 +12,14 @@ import io.restassured.RestAssured;
 import io.restassured.config.SessionConfig;
 import io.restassured.filter.session.SessionFilter;
 import io.restassured.http.ContentType;
-import org.junit.AfterClass;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class LoginResourceIT {
 
-    private static IntegrationTestsSupport support;
+    private IntegrationTestsSupport support;
 
     @BeforeClass
     public static void setUpClass() {
@@ -35,12 +36,89 @@ public class LoginResourceIT {
         RestAssured.baseURI = "http://localhost";
         RestAssured.port = 8080;
         RestAssured.basePath = "/travelplanner/api";
+    }
+
+    @Before
+    public void setUp() {
         support = new IntegrationTestsSupport();
     }
 
-    @AfterClass
-    public static void tearDownClass() {
+    @After
+    public void tearDownClass() {
         support = null;
+    }
+
+    @Test
+    public void testDuplicateRegistration() {
+        given()
+                .contentType(ContentType.URLENC)
+                .formParam("email", support.email())
+                .formParam("password", support.password())
+                .formParam("firstName", support.firstName())
+                .formParam("lastName", "")
+                .accept(ContentType.JSON)
+        .when()
+                .post("/login/register")
+        .then()
+                .statusCode(Status.OK.getStatusCode())
+                .contentType(ContentType.JSON)
+                .body("id", isA(Number.class))
+                .body("updated", notNullValue())
+                .body("email", is(support.email()))
+                .body("password", nullValue())
+                .body("firstName", is(support.firstName()))
+                .body("lastName", is(""));
+
+        given()
+                .contentType(ContentType.URLENC)
+                .formParam("email", support.email())
+                .formParam("password", support.password() + "-other")
+                .formParam("firstName", support.firstName() + "-other")
+                .formParam("lastName", "")
+                .accept(ContentType.JSON)
+        .when()
+                .post("/login/register")
+        .then()
+                .statusCode(Status.CONFLICT.getStatusCode());
+    }
+
+    @Test
+    public void testInvalidRegistrations() {
+        given()
+                .contentType(ContentType.URLENC)
+                .formParam("email", support.email())
+                .formParam("password", "")
+                .formParam("firstName", support.firstName())
+                .formParam("lastName", "")
+                .accept(ContentType.JSON)
+        .when()
+                .post("/login/register")
+        .then()
+                .statusCode(Status.BAD_REQUEST.getStatusCode());
+
+        given()
+                .contentType(ContentType.URLENC)
+                .formParam("email", "")
+                .formParam("password", support.password())
+                .formParam("firstName", support.firstName())
+                .formParam("lastName", "")
+                .accept(ContentType.JSON)
+        .when()
+                .post("/login/register")
+        .then()
+                .statusCode(Status.BAD_REQUEST.getStatusCode());
+
+        given()
+                .contentType(ContentType.URLENC)
+                .formParam("email", support.email())
+                .formParam("password", support.password())
+                .formParam("firstName", "")
+                .formParam("lastName", "")
+                .accept(ContentType.JSON)
+        .when()
+                .post("/login/register")
+        .then()
+                .statusCode(Status.BAD_REQUEST.getStatusCode());
     }
 
     @Test
