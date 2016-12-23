a3724b597c24a96600fd050f27cf9fd50f06cbfb
Miguel García López <miguel.garcia@stelsolutions.com>
2016-12-24 00:59:05 +0100

    Add EJBAccessException as an ApplicationException to map it as FORBIDDEN.

---
+++ server/src/main/java/com/mgl/demo/travelplanner/rest/EJBAccessExceptionMapper.java
@@ -0,0 +1,21 @@
+package com.mgl.demo.travelplanner.rest;
+
+import javax.ejb.EJBAccessException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Provider
+@Slf4j
+public class EJBAccessExceptionMapper implements ExceptionMapper<EJBAccessException> {
+
+    @Override
+    public Response toResponse(EJBAccessException exception) {
+        log.warn("EJB access exception: {}", String.valueOf(exception));
+        return Response.status(Status.FORBIDDEN).build();
+    }
+
+}
