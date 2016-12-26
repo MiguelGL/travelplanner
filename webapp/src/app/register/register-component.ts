208809709105d194496dfddf9423adae89c218bf
Miguel García López <miguel.garcia@stelsolutions.com>
2016-12-26 20:00:10 +0100

    Implement registration client.

--- webapp/src/app/register/register-component.ts
+++ webapp/src/app/register/register-component.ts
@@ -1,7 +1,97 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { TravelplannerApiClientService } from '../shared/travelplanner-api-client/travelplanner-api-client.service';
+import { Message } from 'primeng/components/common/api';
+import { Response } from '@angular/http';
+
 @Component({
   templateUrl: './register-component.html'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
+  registerForm: FormGroup;
+
+  errorMessages: Message[] = [];
+
+  constructor(private formBuilder: FormBuilder,
+              private apiClient: TravelplannerApiClientService,
+              private router: Router) {}
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(64),
+        Validators.pattern('^.+@.+$')
+      ])),
+      firstName: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(64)
+      ])),
+      lastName: new FormControl('', Validators.compose([
+        Validators.maxLength(64)
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(48),
+        Validators.pattern('^[0-9a-zA-Z\_\-]+$')
+      ])),
+      confirmedPassword: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(48),
+        Validators.pattern('^[0-9a-zA-Z\_\-]+$')
+      ])),
+    }, {
+      validator: (group: FormGroup) => {
+        const passwordInput = group.get('password');
+        const confirmPasswordInput = group.get('confirmedPassword');
+        return (passwordInput.value === confirmPasswordInput.value)
+          ? confirmPasswordInput.setErrors(confirmPasswordInput.validator(confirmPasswordInput))
+          : confirmPasswordInput.setErrors({
+             passwordsmismatch: true
+        });
+      }
+    });
+  }
+
+  submit(value: any, valid: boolean, data: any) {
+    if (!valid) return;
+    this.apiClient.register(value.email, value.password, value.firstName, value.lastName)
+      .subscribe((user) => {
+        this.router.navigateByUrl('/login');
+      }, (error) => {
+        if (error instanceof Response) {
+          switch (error.status) {
+            case 400:
+              this.errorMessages = [{
+                severity:'warn', summary:'Registration Error',
+                detail:'Could not register, some data is invalid'
+              }];
+              break;
+            case 409:
+              this.errorMessages = [{
+                severity:'warn', summary:'Registration Error',
+                detail:`Could not register, the email address ${value.email} is already in use`
+              }];
+              break;
+            default:
+              this.errorMessages = [{
+                severity:'error', summary:`Registration Error (${error.status})`,
+                detail:'Could not register, please review entered data'
+              }];
+          }
+        } else {
+          this.errorMessages = [{
+            severity:'error', summary:'Registration Error',
+            detail:'Could register, an unexpected error happened'
+          }]
+        }
+      });
+  }
 
 }
--- webapp/src/app/shared/travelplanner-api-client/travelplanner-api-client.service.ts
+++ webapp/src/app/shared/travelplanner-api-client/travelplanner-api-client.service.ts
@@ -11,6 +11,27 @@ export class TravelplannerApiClientService {
 
   constructor(private http: Http) {}
 
+  register(email: string, password: string,
+           firstName: string, lastName: string = ''): Observable<User> {
+    const params = new URLSearchParams();
+    params.set('email', email);
+    params.set('password', password);
+    params.set('firstName', firstName);
+    params.set('lastName', lastName);
+
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+    return this.http.post('/travelplanner/api/login/register', params.toString(), {headers})
+      .map(response => {
+        if (response.status === 200) {
+          return response.json() as User;
+        } else {
+          throw response;
+        }
+      })
+  }
+
   login(email: string,  password: string): Observable<User> {
     const params = new URLSearchParams();
     params.set('email', email);
@@ -22,7 +43,7 @@ export class TravelplannerApiClientService {
     return this.http.post('/travelplanner/api/login', params.toString(), {headers})
       .map(response => {
         if (response.status === 200) {
-          const user = response.json() as User
+          const user = response.json() as User;
           this.loggedInUser = user;
           return user;
         } else {
