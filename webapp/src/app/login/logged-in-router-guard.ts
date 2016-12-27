e00d782f7bd0edf57e6a74b3091d089f28c216ca
Miguel García López <miguel.garcia@stelsolutions.com>
2016-12-27 20:49:05 +0100

    Fix messed up router guards logic.

--- webapp/src/app/login/logged-in-router-guard.ts
+++ webapp/src/app/login/logged-in-router-guard.ts
@@ -1,16 +1,28 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { TravelplannerApiClientService } from '../shared/travelplanner-api-client/travelplanner-api-client.service';
+import { GlobalMessagesService } from '../shared/global-messages-service/global-messages.service';
 
 @Injectable()
 export class LoggedInRouterGuard implements CanActivate {
 
-  constructor(private apiClient: TravelplannerApiClientService) {}
+  constructor(private router: Router,
+              private apiClient: TravelplannerApiClientService,
+              private messagesService: GlobalMessagesService) {}
 
   canActivate(route: ActivatedRouteSnapshot,
               state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return this.apiClient.canManageUsers;
+    if (this.apiClient.isLoggedIn) {
+      return true;
+    } else {
+      this.messagesService.display({
+        severity: 'info', summary: 'Not Logged In',
+        detail: 'You are not logged in, redirecting to login'
+      })
+      this.router.navigateByUrl('/login');
+      return false;
+    }
   }
 
 }
