98e83a6dcc44ea8593b89b01fec9164be179886b
Miguel García-López <miguel.garcia@tecnicasdeingenieria.com>
2017-04-24 00:02:33 +0200

    Import a missing specific rxjs operator.

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -5,6 +5,9 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
 
+// https://github.com/angular/angular/issues/5632#issuecomment-167026172
+import 'rxjs/add/operator/map'
+
 if (environment.production) {
   enableProdMode();
 }
